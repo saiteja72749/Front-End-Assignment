94f3e5eb65f9be84ccd48b68cc55fa5224ffb666
saiteja72749 <saitejachintu821@gmail.com>
2025-02-08 15:34:44 +0530

    new update

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,6 +2,8 @@ export const fetchRecipes = (query) => async (dispatch) => {
     const appId = process.env.REACT_APP_EDAMAM_APP_ID;
     const appKey = process.env.REACT_APP_EDAMAM_APP_KEY;
   
+    console.log("App ID:", appId);
+    console.log("App Key:", appKey);
     try {
       const response = await fetch(
         `https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}`
@@ -13,7 +15,7 @@ export const fetchRecipes = (query) => async (dispatch) => {
         payload: data.hits.map((hit) => ({
           id: hit.recipe.uri,
           title: hit.recipe.label,
-          image: hit.recipe.image,
+          image: hit.recipe.image
         })),
       });
     } catch (error) {
